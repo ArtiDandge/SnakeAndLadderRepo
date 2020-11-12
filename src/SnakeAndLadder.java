29c0c40331cf50f9764fde6eefb353e8975cdc12
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-12 10:17:56 +0530

    Ensure player get win position 100

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -15,36 +15,7 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Print Welcome Message
-<<<<<<< HEAD
-				System.out.println("Welcome to Snake And Ladder Simulator Program");
-				int Player1Position = 0;
-				int Player1Dice = rollDice();   //player roll dice to get number
-				int option = checkOption(); //player check for options
-				
-				// Use Switch case for option
-				switch (option) {
-					case 1:
-						System.out.println("Players not play");
-						break;
-					case 2:
-						System.out.println("Player got the Ladder");
-						Player1Position += Player1Dice;
-					
-						break;
-					case 3:
-						System.out.println("Oops! Player got the Snake");
-						if ( (Player1Position - Player1Dice) < 0) {
-	                    	Player1Position = 0;
-	                	} else {
-	                    	Player1Position -= Player1Dice;
-	                	}
-					
-						break;
-					default:
-						System.out.println("Something Wrong");
-						break;	
-				}
-=======
+
 		System.out.println("Welcome to Snake And Ladder Simulator Program");
 		int Player1Position = 0;
 		//repeat till win position i.e. till 100
@@ -76,7 +47,7 @@ public class SnakeAndLadder {
 					break;	
 			}
 		}	
->>>>>>> fa638a6ab606884b2f989b139bc966e499829dc7
+
 	}
 
 }
