7c52f65f2840411e046e80d5ae0ec31ac38a2ea6
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-12 09:37:56 +0530

    Game Options for Player(using Switch case): No Play, Ladder and Snake.

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -19,7 +19,31 @@ public class SnakeAndLadder {
 				System.out.println("Welcome to Snake And Ladder Simulator Program");
 				int Player1Position = 0;
 				int Player1Dice = rollDice();   //player roll dice to get number
+				int option = checkOption(); //player check for options
 				
+				// Use Switch case for option
+				switch (option) {
+					case 1:
+						System.out.println("Players not play");
+						break;
+					case 2:
+						System.out.println("Player got the Ladder");
+						Player1Position += Player1Dice;
+					
+						break;
+					case 3:
+						System.out.println("Oops! Player got the Snake");
+						if ( (Player1Position - Player1Dice) < 0) {
+	                    	Player1Position = 0;
+	                	} else {
+	                    	Player1Position -= Player1Dice;
+	                	}
+					
+						break;
+					default:
+						System.out.println("Something Wrong");
+						break;	
+				}
 	}
 
 }
