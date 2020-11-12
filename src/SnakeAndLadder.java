8f292827c1b120902449c314b8ebc4da9dd1aa3f
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-12 09:25:09 +0530

    Roll dice to get number between 1 to 6

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,11 +1,25 @@
 
 public class SnakeAndLadder {
 
+	
+	// Function to roll a dice and get number between 1-6
+	public static int rollDice() {
+		int dicevalue = (int) Math.floor(Math.random()*6) + 1;
+		return dicevalue;
+	}
+	
+	//Function to check Player get a Ladder or snake or player play or not
+	public static int checkOption() {
+		return (int) Math.floor(Math.random()*3) + 1;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Print Welcome Message
 				System.out.println("Welcome to Snake And Ladder Simulator Program");
 				int Player1Position = 0;
+				int Player1Dice = rollDice();   //player roll dice to get number
+				
 	}
 
 }
