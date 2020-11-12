fa638a6ab606884b2f989b139bc966e499829dc7
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-12 10:05:37 +0530

    Check options till player position reach at 100

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,4 +1,3 @@
-
 public class SnakeAndLadder {
 
 	
@@ -8,13 +7,45 @@ public class SnakeAndLadder {
 		return dicevalue;
 	}
 	
+	//Function to check Player get a Ladder or snake or player play or not
+	public static int checkOption() {
+		return (int) Math.floor(Math.random()*3) + 1;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Print Welcome Message
-				System.out.println("Welcome to Snake And Ladder Simulator Program");
-				int Player1Position = 0;
-				int Player1Dice = rollDice();   //player roll dice to get number
+		System.out.println("Welcome to Snake And Ladder Simulator Program");
+		int Player1Position = 0;
+		//repeat till win position i.e. till 100
+		while(Player1Position !=100) {
+			int Player1Dice = rollDice();   //player roll dice to get number
+			int option = checkOption(); //player check for options
+			
+			// Use Switch case for option
+			switch (option) {
+				case 1:
+					System.out.println("Players not play");
+					break;
+				case 2:
+					System.out.println("Player got the Ladder");
+					Player1Position += Player1Dice;
 				
+					break;
+				case 3:
+					System.out.println("Oops! Player got the Snake");
+					if ( (Player1Position - Player1Dice) < 0) {
+                    	Player1Position = 0;
+                	} else {
+                    	Player1Position -= Player1Dice;
+                	}
+				
+					break;
+				default:
+					System.out.println("Something Wrong");
+					break;	
+			}
+		}	
 	}
 
 }
