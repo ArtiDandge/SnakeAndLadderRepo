c43d3677952499a0fa786b55f46b72e07d79706b
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-12 09:16:20 +0530

    Initial commit: Welcome Message

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Print Welcome Message
+				System.out.println("Welcome to Snake And Ladder Simulator Program");
+				int Player1Position = 0;
+	}
+
+}
